dda89f4886c6db6056f7caa25b2d08f305605146
plix <palle.torsson@gmail.com>
2018-03-13 11:53:31 +0100

    adding soundscript

---
+++ unity3d/exempel/scripts/triggerSound.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerSound : MonoBehaviour {
+	public AudioSource source; 
+
+	// Use this for initialization
+	public void Awake () {
+		source = GetComponent<AudioSource> ();
+	}
+
+	public void OnTriggerEnter(Collider other) {
+		source.Play (); 
+	}
+
+}
