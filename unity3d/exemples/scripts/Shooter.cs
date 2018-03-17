11daa9c858e9c59818e42f3675a7762d3f719da6
Khaled <32059353+Khaled90S@users.noreply.github.com>
2018-03-17 12:27:42 +0100

    Add files via upload

---
+++ unity3d/exemples/scripts/Shooter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour {
+
+    public float power = 20f;
+    public float speed = 2f;
+    public Rigidbody bullet;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float h = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float v = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(h, v, 0);
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+            Rigidbody ball = Instantiate(bullet, transform.position, transform.rotation)
+                as Rigidbody;
+            ball.velocity = transform.TransformDirection(Vector3.forward * power);
+        }
+
+
+    }
+}
