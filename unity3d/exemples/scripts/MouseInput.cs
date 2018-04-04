2b4b408beed018ae6e4897eb46b1c3cce6a29fbe
plix <palle.torsson@gmail.com>
2018-04-04 10:27:03 +0200

    adding menu scripts

---
+++ unity3d/exemples/scripts/MouseInput.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseInput : MonoBehaviour {
+
+	float depthIntoScene = 10;
+	public string load; 
+	int r = 0; 
+
+	float defaultDepthIntoScene = 5;
+	float selectScale = .01f;
+
+	//OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider.
+	public void OnMouseDown() {
+		//Get the vector from the camera to the object
+		Vector3 headingToObject = this.transform.position - Camera.main.transform.position;
+		//find the projection on the forward vector of the camera
+		depthIntoScene = Vector3.Dot(headingToObject, Camera.main.transform.forward);
+		Application.LoadLevel(load);
+	}
+
+	//OnMouseOver is called every frame while the mouse is over the GUIElement or Collider.
+	public void OnMouseOver() {
+		//while the mouse is over the object, rotate the object to show it's selected and give us a chance
+		// to inspect all sides of the object.
+		if (r < 100) {
+			this.transform.Rotate (Vector3.up, 2 * Time.deltaTime, Space.Self);
+		} else {
+			this.transform.Rotate (Vector3.up, -2 * Time.deltaTime, Space.Self);
+		}
+		if (r > 200) {
+			r = 0; 
+		}
+		r++; 
+		Debug.Log (r); 
+	}
+	
+
+}
---
+++ unity3d/exemples/scripts/moveMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveMove : MonoBehaviour {
+
+	float depthIntoScene = 10;
+
+	// Update is called once per frame
+	void Update () {
+		MoveToMouseAtObjectDepth();
+	}
+
+	void MoveToMouseAtObjectDepth() {
+		Vector3 mouseScreenPosition = Input.mousePosition;
+
+		//create a ray that goes into the scene from the camera, through the mouse position
+		Ray ray = Camera.main.ScreenPointToRay(mouseScreenPosition);
+		float depth;
+		RaycastHit hitInfo; //create a variable to store information about the object hit (if any)
+
+		//Check to see if the ray hits any objects in the scene
+		//Also pass in hitInfo, so that Raycast can store the information about the hit there
+		//The out keyword is a parameter modifier used to tell C# that this object should be passed by reference, instead of by value
+		//basically it makes it so we can properly access hitInfo.
+		//It's important to note that the objects we're hoping to hit with our ray must have a collider component attached to them
+		if (Physics.Raycast (ray, out hitInfo)) 
+		{
+			if (hitInfo.transform.tag == "menu") {
+				Debug.Log ("menu");
+				
+			} else {
+				Destroy(hitInfo.collider.gameObject);
+			}
+			//Move this object to the postion we hit.
+			this.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y, hitInfo.point.z);
+
+		} else {
+			//if we didn't hit anything, set the depth to the arbitrary depth
+			depth = depthIntoScene;
+			//now we can reuse our previous code to position the object using the depth we defined here
+			MoveToMouseAtSpecifiedDepth(depth);
+		}
+	}
+
+	void MoveToMouseAtSpecifiedDepth(float depth) {
+		Vector3 mouseScreenPosition = Input.mousePosition;
+		mouseScreenPosition.z = depth;
+		Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mouseScreenPosition);
+		this.transform.position = new Vector3(mouseWorldPosition.x, mouseWorldPosition.y, mouseWorldPosition.z);
+	}
+}
