96c372a4db0d1b96f54a5d118a8b46553a2af1a7
plix <palle.torsson@gmail.com>
2018-09-23 16:25:00 +0200

    run comment python

--- arduino/xyz_mup6050/xyz_mup6050.ino
+++ arduino/xyz_mup6050/xyz_mup6050.ino
@@ -82,7 +82,7 @@ void setup() {
     // initialize serial communication
     // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
     // it's really up to you depending on your project)
-    Serial.begin(38400);
+    Serial.begin(9600);
 
     // initialize device
     Serial.println("Initializing I2C devices...");
---
+++ python /runComments.py
@@ -0,0 +1,13 @@
+# skapa en variabel och initiera den "hello", 4 - dynamiskt typat
+# definiera en funktion, indentering
+# funktion som tar argument
+# funktion som returnerar ett värde
+# funktion med standardvärdet för ett argument
+# funktion med villkorligt flöde - if, elif, else
+# skapa list och läs den med loop
+# main konstruktion
+# importera lista från annan fil
+# läs och skriv till fil
+# läs webbsida
+# läs json
+# spara json
