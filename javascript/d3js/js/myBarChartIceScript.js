e006263319f6ef1f9a1eff20cadaae7458c66f02
plix <palle.torsson@gmail.com>
2018-10-24 09:43:36 +0200

    adding d3 exemples

---
+++ javascript/d3js/js/myBarChartIceScript.js
@@ -0,0 +1,128 @@
+
+d3.json('./data/arctic-sea-ice-extent-4.exhibit.json').then(function(d) {
+
+  var w = window.outerWidth;
+
+  var temperatures = [],
+      dates = [],
+      margin = { top: 10, right: 0, bottom: 30, left: 20 },
+      height = 400 - margin.top - margin.bottom,
+      width = w - margin.left - margin.right - 140;
+
+  var   tempColor,
+        yScale,
+        yAxisValues,
+        yAxisTicks,
+        yGuide,
+        xScale,
+        xAxisValues,
+        xAxisTicks,
+        xGuide,
+        colors,
+        tooltip,
+        myChart;
+
+  var d = d.items; 
+
+  for (var i = 0; i<d.length; i++) {
+    temperatures.push(d[i].september);
+    dates.push(d[i].year);
+  }
+
+  var tMax = d3.max(temperatures); 
+
+  yScale = d3.scaleLinear()
+    .domain([0, tMax])
+    .range([0,height]);
+
+  yAxisValues = d3.scaleLinear()
+    .domain([0, tMax])
+    .range([height,0]);
+
+  yAxisTicks = d3.axisLeft(yAxisValues)
+    .ticks(10)
+
+  xScale = d3.scaleBand()
+    .domain(temperatures)
+    .paddingInner(.1)
+    .paddingOuter(.1)
+    .range([0, width])
+
+  xAxisValues = d3.scaleTime()
+    .domain([dates[0],dates[(dates.length-1)]])
+    .range([0, width])
+
+  xAxisTicks = d3.axisBottom(xAxisValues)
+    .ticks(10)
+
+  colors = d3.scaleLinear()
+    .domain([4, 6, d3.max(temperatures)])
+    .range(['yellow', 'pink', 'green'])
+
+  tooltip = d3.select('body')
+    .append('div')
+    .style('position', 'absolute')
+    .style('padding', '0 10px')
+    .style('background', 'white')
+    .style('opacity', 0);
+
+  myChart = d3.select('#viz').append('svg')
+    .attr('width', width + margin.left + margin.right)
+    .attr('height', height + margin.top + margin.bottom)
+    .append('g')
+    .attr('transform',
+      'translate(' + margin.left + ',' + margin.right + ')')
+    .selectAll('rect').data(temperatures)
+    .enter().append('rect')
+      .attr('fill', colors)
+      .attr('width', function(d) {
+        return xScale.bandwidth();
+      })
+      .attr('height', 0)
+      .attr('x', function(d) {
+        return xScale(d);
+      })
+      .attr('y', height)
+      .on('mouseover', function(d) {
+        tooltip.transition().duration(100)
+          .style('opacity', .9)
+        tooltip.html(
+          '<div style="font-size: 1.2rem; font-weight: bold">' +
+            d + '.000 m2</div>'
+        )
+          .style('left', (d3.event.pageX -35) + 'px')
+          .style('top', (height+20) + 'px')
+          .style('zIndex', 1000)
+        tempColor = this.style.fill;
+        d3.select(this)
+          .style('fill', 'lightgray')
+      })
+      .on('mouseout', function(d) {
+        tooltip.html('')
+        d3.select(this)
+          .style('fill', tempColor)
+      });
+      console.log(myChart);
+
+  yGuide = d3.select('#viz svg').append('g')
+            .attr('transform', 'translate(20,0)')
+            .call(yAxisTicks)
+
+  xGuide = d3.select('#viz svg').append('g')
+            .attr('transform', 'translate(20,'+ height + ')')
+            .call(xAxisTicks)
+
+  myChart.transition()
+    .attr('height', function(d) {
+      return yScale(d);
+    })
+    .attr('y', function(d) {
+      return height - yScale(d);
+    })
+    .delay(function(d, i) {
+      return i * 20;
+    })
+    .duration(1000)
+    .ease(d3.easeBounceOut)
+
+}); // json import
---
+++ javascript/d3js/js/pulsarScript.js
@@ -0,0 +1,91 @@
+
+var height = 700
+  , width = height / 1.138
+  , top_margin = ~~( width / 13 )+20
+  , amplitude = width / 350
+  ;
+
+var svg = d3.select( 'body' ).append( 'svg' )
+    .attr( 'width', 960 )
+    .attr( 'height', height + top_margin + 10 + 10 )
+    .append( 'g' )
+    .attr( 'transform', `translate(${960/2-width/2},${10})` );
+
+// ../data/pulsar.csv
+// https://gist.githubusercontent.com/borgar/31c1e476b8e92a11d7e9/raw/0fae97dab6830ecee185a63c1cee0008f6778ff6/pulsar.csv', function ( raw ) {
+  
+var inter = setInterval(function() {
+                    updateData();
+            }, 5000);
+
+function updateData()  {
+d3.text('../data/pulsar.csv', function ( raw ) {
+  
+  var data = d3.csv.parseRows( raw, r => r.map( Number ) ),
+      x = d3.scale.linear()
+            .domain([ 0, data[0].length - 1 ])
+            .range([ 0, width ]),
+      y = d3.scale.linear()
+            .domain([ 0, data.length - 1 ])
+            .range([ top_margin, height ]);
+
+  var line = d3.svg.line()
+      .x( (d,i) => x( i ) )
+      .y( (d,i) => -d -Math.random(window.screenX) * amplitude );
+  
+  svg.append( 'g' )
+    .selectAll( '.wave' ).data( data ).enter()
+      .append( 'path' )
+        .attr( 'transform', (d,i) => `translate(0,${y(i)})` )
+        .attr( 'class', 'wave' )
+        .attr( 'd', line )
+        ;
+
+  // top text
+  svg.append( 'text' )
+    .text( 'PRS B1919+21' )
+    .attr( 'class', 'title' )
+    .attr( 'x', 0 )
+    .attr( 'dy', '.9em' );
+
+  svg.append( 'text' )
+    .text( '318 MHz' )
+    .attr( 'class', 'freq' )
+    .attr( 'x', width )
+    .attr( 'dy', '.9em' );
+
+  // bottom axis
+  var axis = svg.append( 'g' )
+    .attr( 'class', 'axis' )
+    .attr( 'transform', `translate(0,${height+15})` );
+
+  // the pulse-width is cropped to 92ms
+  var ax = d3.scale.linear().domain([ 0, 92 ])
+    .range([ 0, width ]);
+
+  axis.append( 'line' )
+    .attr( 'x2', width );
+
+  axis.selectAll( '.tick' )
+    .data( d3.range( 5, 92, 20 ) ).enter()
+    .append( 'line' )
+      .attr( 'x1', ax )
+      .attr( 'x2', ax )
+      .attr( 'y1', -5 );
+
+  axis.append( 'path' )
+    .attr( 'd', `M0,0 L0,9.5
+                 M0,5 L${ax(5)},5
+                 M${ax(20)},0 L${ax(20)},9.5
+                 M${ax(15)},5 L${ax(20)},5` )
+    .attr( 'transform', `translate(${ax(45)},5)` );
+
+  axis.append( 'text' )
+    .text( '20 ms' )
+    .attr( 'x', ax( 5 + 50 ) )
+    .attr( 'y', 3 )
+    .attr( 'dy', '.92em' );
+
+});
+}
+
