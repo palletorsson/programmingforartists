bb926441a58bb4fc8399516b64b9e3278cb8a57b
plix <palle.torsson@gmail.com>
2018-10-24 21:59:11 +0200

    base script

---
+++ javascript/threejs/js/myBaseScript.js
@@ -0,0 +1,15 @@
+// skapa en scen, en kamera och en renderare
+
+// scala bilden till window-size och lägg till allt till dom:en
+
+// skapa en kub och ett material till kub
+
+// sammanfoga dessa i en mech som består av kuben och materialet
+
+// lägg till kuben till scenen
+
+// sätt kamerans position 
+
+// rotera kuben och rendera scenen 
+
+// starta animationen 
