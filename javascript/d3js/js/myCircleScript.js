b750598a0bcd061d467ca80ff6759cff6bf5682e
plix <palle.torsson@gmail.com>
2018-10-25 11:11:23 +0200

    adding circle script

--- javascript/d3js/js/myCircleScript.js
+++ javascript/d3js/js/myCircleScript.js
@@ -1,37 +1,6 @@
-var textString = " .. ... .... Level of thinking that occurs is influenced by the level of questions asked"; 
-var index = 0; 
-var splitText = textString.split(" "); 
-var len = splitText.length-1; 
+
 var showAnim = 5; 
 
-function showText() {
-d3.selectAll("svg")
-  .remove()
-
-d3.selectAll("p")
-  .remove()
-
-d3.select("body").select("#vis")
-    .text(splitText[index])
-    .style("text-align", "center")
-    .style("line-height", "320px")
-    .style("font-size", "100px")
-    .style("transform", "rotate(-180deg) scale(0.001, 0.001)")
-  .transition()
-    .duration(2000)
-    .style("transform", null);
-
-  index = index + 1; 
-  if (index > len)	 {
-  	clearTimeout(loop);
-  }
-}
-
-if (showAnim == 1) {
-	var loop = setInterval(function() {
-		showText(); 
-	}, 700); 
-}
 
 if (showAnim == 2) {
 d3.selectAll("svg")
