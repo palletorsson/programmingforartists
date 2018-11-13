91bf533d0fb6224677c426d693b119465f2de806
plix <palle.torsson@gmail.com>
2018-11-13 16:42:12 +0100

    Update unity3d/exemples/scripts/triggerCapsuleAnim.cs

---
+++ unity3d/exemples/scripts/triggerCapsuleAnim.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class triggerCapsuleAnim : MonoBehaviour {
+
+	private Animator anim;	
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			Debug.Log("Moving");
+			anim.Play("CapsuleAnimation");
+			anim.speed = 2;
+		}
+		if (Input.GetKeyDown (KeyCode.G)) {
+			Debug.Log ("Stopping");
+			anim.speed = 0;
+		}
+	}
+}		
