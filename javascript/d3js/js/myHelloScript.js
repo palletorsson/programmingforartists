25e3e33db3f598802c78405b591a04fb29f3b310
plix <palle.torsson@gmail.com>
2019-10-20 22:43:48 +0200

    Update myHelloScript.js

--- javascript/d3js/js/myHelloScript.js
+++ javascript/d3js/js/myHelloScript.js
@@ -1,11 +1,9 @@
-d3.select("#vis")
-    .text("T-shirt text") // del 1
+d3.select("body")
+    .text("Hello World")
     .style("text-align", "center")
     .style("line-height", "320px")
     .style("font-size", "100px")
-    .style("transform", "rotate(-180deg) scale(0.001, 0.001)") //del 2
-    .transition() // del 3
-    .duration(2000) // del 4
+    .style("transform", "rotate(-180deg) scale(0.001, 0.001)")
+    .transition()
+    .duration(2000)
     .style("transform", null);
-
-/*  */
