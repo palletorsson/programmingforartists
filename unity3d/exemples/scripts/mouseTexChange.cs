485abffcb643af66e591fea11d48cafde56b8a76
plix <palle.torsson@gmail.com>
2019-10-22 15:05:59 +0200

    Create mouseTexChange.cs

---
+++ unity3d/exemples/scripts/mouseTexChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseTexChange : MonoBehaviour
+{
+
+    public Texture[] textures;
+    public Renderer rend;
+    private int index = 0; 
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            index = index + 1;
+        }
+     
+        if (index > textures.Length - 1)
+        {
+            index = 0;
+        }
+       
+        rend.material.mainTexture = textures[index];
+
+    }
+}
